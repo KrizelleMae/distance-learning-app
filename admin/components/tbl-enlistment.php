76886edb67386cf4342e006a89e5baf5f0913c44
Krizelle Mae B. Falcasantos <krizellemaeb.falcasantos@gmail.com>
2022-03-06 22:05:22 +0800

    New commit

---
+++ admin/components/tbl-enlistment.php
@@ -0,0 +1,103 @@
+<div class="">
+  <div class="flex flex-col">
+    <div class="my-2 overflow-x-auto sm:-mx-8 m-0">
+      <div
+        class="py-2 align-middle inline-block min-w-full sm:px-6 py-5 lg:px-8"
+      >
+        <div
+          class="shadow-md overflow-hidden border-b border-gray-200 sm:rounded"
+        >
+          <table class="table-auto min-w-full divide-y divide-gray-200">
+            <thead class="bg-blue py-0">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-5 text-left text-sm font-medium text-white uppercase tracking-wider"
+                >
+                  Student ID
+                </th>
+
+                <th
+                  scope="col"
+                  class="px-6 py-5 text-left text-sm font-medium text-white uppercase tracking-wider"
+                >
+                  Student name
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-5 text-left text-sm font-medium text-white uppercase tracking-wider"
+                >
+                  Email
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-5 text-left text-sm font-medium text-white uppercase tracking-wider"
+                >
+                  program
+                </th>
+
+                <th
+                  scope="col"
+                  class="px-6 py-5 text-left text-sm font-medium text-white uppercase tracking-wider"
+                >
+                  Year & Section
+                </th>
+
+                <th
+                  scope="col"
+                  class="px-6 py-5 text-left text-sm font-medium text-white uppercase tracking-wider"
+                >
+                  Remarks
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr>
+                <td class="px-2 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="text-sm font-bold text-gray-900">
+                        12345678
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-5 py-3 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">
+                    Krizelle Mae Falcasantos
+                  </div>
+
+                  <!-- <div class="text-md text-white">
+                                    Optimization
+                                 </div> -->
+                </td>
+                <td class="px-5 py-5 whitespace-nowrap text-md text-gray-900">
+                  <div class="text-sm text-gray-900">random@gmail.com</div>
+                </td>
+                <td class="px-5 py-3 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">Masters in Education</div>
+                </td>
+
+                <td class="px-5 py-3 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">Masteral - BEED 1A</div>
+                </td>
+
+                <!-- <td class="px-5 py-3 whitespace-nowrap">
+                                 <span
+                                    class="px-2 inline-flex text-md leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                                 >
+                                    Active
+                                 </span>
+                              </td> -->
+
+                <td class="px-5 py-3 whitespace-nowrap">
+                  <div class="text-xs text-gray-800">Remarks here ----</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
